3b3069ecf3591a5617238924e4074ac410989768
eliastellati <eliastellatibvb@gmail.com>
2021-07-13 16:15:07 +0200

    br

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hello World
+                    Hello World <br>
                     Il mio primo progetto Laravel
                 </div>
 
